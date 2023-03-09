cce18c54922a856e0faad8a87561600828008308
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-09 09:59:42 +0100

    added test card

--- lib/ui/dashboard/front_layer.dart
+++ lib/ui/dashboard/front_layer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
 import 'package:mayberks_me/components/dashboard_profile_circle.dart';
 import 'package:mayberks_me/components/dashboard_profile_social_media.dart';
 import 'package:mayberks_me/components/dashobard_hey_text.dart';
@@ -32,6 +33,35 @@ class DashboardFrontLayer extends StatelessWidget {
           height: context.dynamicHeight(0.017),
         ),
         DashboardHeyText(),
+        SizedBox(
+          height: context.dynamicHeight(0.05),
+        ),
+        SizedBox(
+          width: context.dynamicWidth(0.15),
+          height: context.dynamicHeight(0.9),
+          child: GFCard(
+            boxFit: BoxFit.cover,
+            borderRadius: BorderRadius.circular(35.0),
+            image: Image.asset('assets/images/completed_commission.png'),
+            title: GFListTile(
+              avatar: GFAvatar(),
+              titleText: 'Game Controllers',
+              subTitleText: 'PlayStation 4',
+            ),
+            content: Text("Some quick example text to build on the card"),
+            buttonBar: GFButtonBar(
+              children: <Widget>[
+                GFAvatar(
+                  backgroundColor: GFColors.PRIMARY,
+                  child: Icon(
+                    Icons.share,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
