7c690b904c0b82598a40e6e6eea45b0576ce3721
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-09 04:48:34 +0100

    color and theme settings

---
+++ lib/constants/color.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const mainTextColor = Color.fromRGBO(112, 112, 112, 1);
+const mainLightTextColor = Color.fromRGBO(85, 85, 85, 1);
+
---
+++ lib/constants/theme_settings.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const mainDefaultPadding = 20;
+
+final mainDefaultShadow = BoxShadow(
+  offset: Offset(0, 50),
+  blurRadius: 50,
+  color: Color.fromRGBO(7, 0, 177, 1).withOpacity(0.15),
+);
+
+final mainDefaultCardShadow = BoxShadow(
+  offset: Offset(0, 20),
+  blurRadius: 50,
+  color: Colors.black.withOpacity(0.1),
+);
+
+final mainDefaultInputDecoration = InputDecorationTheme(
+  border: mainDefaultOutlineInput,
+  enabledBorder: mainDefaultOutlineInput,
+  focusedBorder: mainDefaultOutlineInput,
+);
+
+final mainDefaultOutlineInput = OutlineInputBorder(
+  borderSide: BorderSide(
+    color: Color.fromRGBO(206, 228, 253, 1),
+  ),
+);
