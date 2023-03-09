37397a6fdf015bc46fdbd7360264c900c430d2f6
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-09 08:04:48 +0100

    update models

--- lib/models/statistics.dart
+++ lib/models/statistics.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 
 class Statistic {
   final int id;
-  final String title;
-  final String image;
+  final String title, subtitle, image;
   final Color color;
 
   Statistic({
     required this.id,
     required this.title,
+    required this.subtitle,
     required this.image,
     required this.color,
   });
@@ -18,6 +18,7 @@ List<Statistic> statistic = [
   Statistic(
     id: 1,
     title: "Completed Commission",
+    subtitle: "750",
     image: "",
     color: Color.fromARGB(255, 217, 255, 252),
   ),
