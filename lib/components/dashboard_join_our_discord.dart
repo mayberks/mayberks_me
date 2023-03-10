f06c765ee4c465ae89da6425298ce18949749eda
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-11 00:07:29 +0100

    optimization

---
+++ lib/components/dashboard_join_our_discord.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mayberks_me/constants/extensions.dart';
+
+class DashboardJoinOurDiscord extends StatelessWidget {
+  const DashboardJoinOurDiscord({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Spacer(
+          flex: 9,
+        ),
+        Expanded(
+          flex: 17,
+          child: Container(
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 40, 40, 40),
+              borderRadius: BorderRadius.all(
+                Radius.circular(15),
+              ),
+            ),
+            width: context.dynamicWidth(0.5),
+            height: context.dynamicHeight(0.10),
+            child: Row(
+              children: [
+                const Spacer(
+                  flex: 30,
+                ),
+                const Expanded(
+                  flex: 30,
+                  child: FaIcon(
+                    FontAwesomeIcons.discord,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                ),
+                Expanded(
+                  flex: 300,
+                  child: Text(
+                    'Ready to join our Service & Company community server?',
+                    style: GoogleFonts.karla(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w200,
+                      fontSize: 23,
+                    ),
+                  ),
+                ),
+                const Spacer(
+                  flex: 10,
+                ),
+                Expanded(
+                  flex: 70,
+                  child: SizedBox(
+                    height: context.dynamicHeight(0.04),
+                    width: context.dynamicWidth(0.14),
+                    child: OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        side: const BorderSide(
+                          color: Colors.transparent,
+                        ),
+                        backgroundColor: const Color.fromARGB(255, 20, 20, 20),
+                        foregroundColor: Colors.white,
+                        textStyle: GoogleFonts.karla(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 20,
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text('Join!'),
+                    ),
+                  ),
+                ),
+                const Spacer(
+                  flex: 30,
+                ),
+              ],
+            ),
+          ),
+        ),
+        const Spacer(
+          flex: 10,
+        ),
+      ],
+    );
+  }
+}
--- lib/ui/dashboard/front_layer.dart
+++ lib/ui/dashboard/front_layer.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:mayberks_me/components/dashboard_all_works.dart';
 import 'package:mayberks_me/components/dashboard_hire_discord_button.dart';
+import 'package:mayberks_me/components/dashboard_join_our_discord.dart';
 import 'package:mayberks_me/components/dashboard_profile_circle.dart';
 import 'package:mayberks_me/components/dashboard_profile_social_media.dart';
 import 'package:mayberks_me/components/dashboard_some_places_text.dart';
@@ -52,83 +51,7 @@ class DashboardFrontLayer extends StatelessWidget {
         SizedBox(
           height: context.dynamicHeight(0.05),
         ),
-        Row(
-          children: [
-            const Spacer(
-              flex: 9,
-            ),
-            Expanded(
-              flex: 17,
-              child: Container(
-                decoration: const BoxDecoration(
-                  color: Color.fromARGB(255, 40, 40, 40),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(15),
-                  ),
-                ),
-                width: context.dynamicWidth(0.5),
-                height: context.dynamicHeight(0.10),
-                child: Row(
-                  children: [
-                    const Spacer(
-                      flex: 30,
-                    ),
-                    const Expanded(
-                      flex: 30,
-                      child: FaIcon(
-                        FontAwesomeIcons.discord,
-                        color: Colors.white,
-                        size: 30,
-                      ),
-                    ),
-                    Expanded(
-                      flex: 300,
-                      child: Text(
-                        'Ready to join our Service & Company community server?',
-                        style: GoogleFonts.karla(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w200,
-                          fontSize: 23,
-                        ),
-                      ),
-                    ),
-                    const Spacer(
-                      flex: 10,
-                    ),
-                    Expanded(
-                      flex: 70,
-                      child: SizedBox(
-                        height: context.dynamicHeight(0.04),
-                        width: context.dynamicWidth(0.14),
-                        child: OutlinedButton(
-                          style: OutlinedButton.styleFrom(
-                            side: const BorderSide(
-                              color: Colors.white,
-                            ),
-                            backgroundColor: const Color.fromARGB(255, 20, 20, 20),
-                            foregroundColor: Colors.white,
-                            textStyle: GoogleFonts.karla(
-                              fontWeight: FontWeight.w300,
-                              fontSize: 20,
-                            ),
-                          ),
-                          onPressed: () {},
-                          child: const Text('Join!'),
-                        ),
-                      ),
-                    ),
-                    const Spacer(
-                      flex: 30,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            const Spacer(
-              flex: 10,
-            ),
-          ],
-        ),
+        const DashboardJoinOurDiscord(),
         SizedBox(
           height: context.dynamicHeight(0.1),
         ),
