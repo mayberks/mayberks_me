c364a980480b4764c06e90499829682dc3ccd2d5
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-10 16:43:58 +0100

    adding x2 test button

--- lib/ui/dashboard/front_layer.dart
+++ lib/ui/dashboard/front_layer.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:getwidget/components/button/gf_button.dart';
+import 'package:getwidget/types/gf_button_type.dart';
 import 'package:mayberks_me/components/dashboard_profile_circle.dart';
 import 'package:mayberks_me/components/dashboard_profile_social_media.dart';
 import 'package:mayberks_me/components/dashobard_hey_text.dart';
 import 'package:mayberks_me/constants/extensions.dart';
-import 'package:mayberks_me/constants/subtitle.dart';
-import 'package:morphing_text/morphing_text.dart';
 
 import '../../components/dashboard_profile_name_text.dart';
 
@@ -38,9 +38,25 @@ class DashboardFrontLayer extends StatelessWidget {
         SizedBox(
           height: context.dynamicHeight(0.05),
         ),
-        SizedBox(
-          width: context.dynamicWidth(0.15),
-          height: context.dynamicHeight(0.9),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            GFButton(
+              onPressed: () {},
+              text: "Hire Me!",
+              type: GFButtonType.outline2x,
+              icon: FaIcon(FontAwesomeIcons.gitAlt),
+            ),
+            SizedBox(
+              width: context.dynamicWidth(0.1),
+            ),
+            GFButton(
+              onPressed: () {},
+              text: "Contact Me!",
+              type: GFButtonType.outline,
+            ),
+          ],
         ),
       ],
     );
