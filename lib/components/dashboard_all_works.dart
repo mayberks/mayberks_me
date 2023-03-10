95ec0aefee3363acd3c8f4b6eba005f0db32ae25
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-10 23:27:24 +0100

    optimization

---
+++ lib/components/dashboard_all_works.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:mayberks_me/components/works/ada_dogaltas.dart';
+import 'package:mayberks_me/components/works/glory_hosting_solutions.dart';
+import 'package:mayberks_me/components/works/infumia_limited.dart';
+import 'package:mayberks_me/components/works/senior_team.dart';
+import 'package:mayberks_me/components/works/valatic.dart';
+import 'package:mayberks_me/constants/extensions.dart';
+
+class DashboardAllWorks extends StatelessWidget {
+  const DashboardAllWorks({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return         Column(
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const InfumiaLimited(),
+            SizedBox(
+              width: context.dynamicWidth(0.03),
+            ),
+            const GloryHostingSolutions(),
+            SizedBox(
+              width: context.dynamicWidth(0.03),
+            ),
+            const SeniorTeam(),
+          ],
+        ),
+        SizedBox(
+          height: context.dynamicHeight(0.03),
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const AdaDogaltas(),
+            SizedBox(
+              width: context.dynamicWidth(0.03),
+            ),
+            const Valatic(),
+          ],
+        )
+      ],
+    );
+  }
+}
--- lib/ui/dashboard/front_layer.dart
+++ lib/ui/dashboard/front_layer.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:mayberks_me/components/dashboard_all_works.dart';
 import 'package:mayberks_me/components/dashboard_hire_discord_button.dart';
 import 'package:mayberks_me/components/dashboard_profile_circle.dart';
 import 'package:mayberks_me/components/dashboard_profile_social_media.dart';
 import 'package:mayberks_me/components/dashboard_some_places_text.dart';
 import 'package:mayberks_me/components/dashobard_hey_text.dart';
-import 'package:mayberks_me/components/works/ada_dogaltas.dart';
-import 'package:mayberks_me/components/works/glory_hosting_solutions.dart';
-import 'package:mayberks_me/components/works/infumia_limited.dart';
-import 'package:mayberks_me/components/works/senior_team.dart';
-import 'package:mayberks_me/components/works/valatic.dart';
 import 'package:mayberks_me/constants/extensions.dart';
 
 import '../../components/dashboard_profile_name_text.dart';
@@ -50,43 +46,7 @@ class DashboardFrontLayer extends StatelessWidget {
         SizedBox(
           height: context.dynamicHeight(0.05),
         ),
-
-        /// SizedBox(
-        ///   width: context.dynamicWidth(0.03),
-        /// ),
-        Column(
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const InfumiaLimited(),
-                SizedBox(
-                  width: context.dynamicWidth(0.03),
-                ),
-                const GloryHostingSolutions(),
-                SizedBox(
-                  width: context.dynamicWidth(0.03),
-                ),
-                const SeniorTeam(),
-              ],
-            ),
-            SizedBox(
-              height: context.dynamicHeight(0.03),
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const AdaDogaltas(),
-                SizedBox(
-                  width: context.dynamicWidth(0.03),
-                ),
-                const Valatic(),
-              ],
-            )
-          ],
-        ),
+        const DashboardAllWorks(),
         SizedBox(
           height: context.dynamicHeight(0.5),
         ),
