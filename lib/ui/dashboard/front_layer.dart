415735597d70c72648d5b2865cde387bae2e8360
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-10 22:40:47 +0100

    add works

--- lib/ui/dashboard/front_layer.dart
+++ lib/ui/dashboard/front_layer.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:mayberks_me/components/dashboard_hire_discord_button.dart';
 import 'package:mayberks_me/components/dashboard_profile_circle.dart';
 import 'package:mayberks_me/components/dashboard_profile_social_media.dart';
 import 'package:mayberks_me/components/dashboard_some_places_text.dart';
 import 'package:mayberks_me/components/dashobard_hey_text.dart';
+import 'package:mayberks_me/components/works/infumia_limited.dart';
 import 'package:mayberks_me/constants/extensions.dart';
 
 import '../../components/dashboard_profile_name_text.dart';
@@ -46,274 +46,19 @@ class DashboardFrontLayer extends StatelessWidget {
         SizedBox(
           height: context.dynamicHeight(0.05),
         ),
+
+        /// SizedBox(
+        ///   width: context.dynamicWidth(0.03),
+        /// ),
         Column(
           children: [
             Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  height: context.dynamicHeight(0.5),
-                  width: context.dynamicWidth(0.15),
-                  child: Column(
-                    children: [
-                      const CircleAvatar(
-                        minRadius: 30,
-                        backgroundColor: Colors.white,
-                        child: CircleAvatar(
-                          radius: 30,
-                          backgroundImage: AssetImage(
-                              'assets/images/infumia_limited_logo.jpg'),
-                        ),
-                      ),
-                      SizedBox(
-                        height: context.dynamicHeight(0.02),
-                      ),
-                      Text(
-                        'Infumia Limited',
-                        style: GoogleFonts.rubik(
-                          color: const Color.fromRGBO(255, 139, 19, 1),
-                          fontWeight: FontWeight.w800,
-                          fontSize: 16,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 7,
-                      ),
-                      Text(
-                        'Software Company',
-                        style: GoogleFonts.rubik(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w100,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 7,
-                      ),
-                      Text(
-                        'I have been working as a co-founder and software developer of Infumia software and graphic design company since 2019.',
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.rubik(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  width: context.dynamicWidth(0.03),
-                ),
-                Container(
-                  height: context.dynamicHeight(0.5),
-                  width: context.dynamicWidth(0.15),
-                  child: Column(
-                    children: [
-                      const CircleAvatar(
-                        minRadius: 30,
-                        backgroundColor: Colors.white,
-                        child: CircleAvatar(
-                          radius: 30,
-                          backgroundImage: AssetImage(
-                              'assets/images/infumia_limited_logo.jpg'),
-                        ),
-                      ),
-                      SizedBox(
-                        height: context.dynamicHeight(0.02),
-                      ),
-                      Text(
-                        'Infumia Limited',
-                        style: GoogleFonts.rubik(
-                          color: const Color.fromRGBO(255, 139, 19, 1),
-                          fontWeight: FontWeight.w800,
-                          fontSize: 16,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 7,
-                      ),
-                      Text(
-                        'Software Company',
-                        style: GoogleFonts.rubik(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w100,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 7,
-                      ),
-                      Text(
-                        'I have been working as a co-founder and software developer of Infumia software and graphic design company since 2019.',
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.rubik(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  width: context.dynamicWidth(0.03),
-                ),
-                Container(
-                  height: context.dynamicHeight(0.5),
-                  width: context.dynamicWidth(0.15),
-                  child: Column(
-                    children: [
-                      const CircleAvatar(
-                        minRadius: 30,
-                        backgroundColor: Colors.white,
-                        child: CircleAvatar(
-                          radius: 30,
-                          backgroundImage: AssetImage(
-                              'assets/images/infumia_limited_logo.jpg'),
-                        ),
-                      ),
-                      SizedBox(
-                        height: context.dynamicHeight(0.02),
-                      ),
-                      Text(
-                        'Infumia Limited',
-                        style: GoogleFonts.rubik(
-                          color: const Color.fromRGBO(255, 139, 19, 1),
-                          fontWeight: FontWeight.w800,
-                          fontSize: 16,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 7,
-                      ),
-                      Text(
-                        'Software Company',
-                        style: GoogleFonts.rubik(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w100,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 7,
-                      ),
-                      Text(
-                        'I have been working as a co-founder and software developer of Infumia software and graphic design company since 2019.',
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.rubik(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                InfumiaLimited(),
               ],
             ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: context.dynamicHeight(0.5),
-                  width: context.dynamicWidth(0.15),
-                  child: Column(
-                    children: [
-                      const CircleAvatar(
-                        minRadius: 30,
-                        backgroundColor: Colors.white,
-                        child: CircleAvatar(
-                          radius: 30,
-                          backgroundImage: AssetImage(
-                              'assets/images/infumia_limited_logo.jpg'),
-                        ),
-                      ),
-                      SizedBox(
-                        height: context.dynamicHeight(0.02),
-                      ),
-                      Text(
-                        'Infumia Limited',
-                        style: GoogleFonts.rubik(
-                          color: const Color.fromRGBO(255, 139, 19, 1),
-                          fontWeight: FontWeight.w800,
-                          fontSize: 16,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 7,
-                      ),
-                      Text(
-                        'Software Company',
-                        style: GoogleFonts.rubik(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w100,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 7,
-                      ),
-                      Text(
-                        'I have been working as a co-founder and software developer of Infumia software and graphic design company since 2019.',
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.rubik(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  width: context.dynamicWidth(0.03),
-                ),
-                Container(
-                  height: context.dynamicHeight(0.5),
-                  width: context.dynamicWidth(0.15),
-                  child: Column(
-                    children: [
-                      const CircleAvatar(
-                        minRadius: 30,
-                        backgroundColor: Colors.white,
-                        child: CircleAvatar(
-                          radius: 30,
-                          backgroundImage: AssetImage(
-                              'assets/images/infumia_limited_logo.jpg'),
-                        ),
-                      ),
-                      SizedBox(
-                        height: context.dynamicHeight(0.02),
-                      ),
-                      Text(
-                        'Infumia Limited',
-                        style: GoogleFonts.rubik(
-                          color: const Color.fromRGBO(255, 139, 19, 1),
-                          fontWeight: FontWeight.w800,
-                          fontSize: 16,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 7,
-                      ),
-                      Text(
-                        'Software Company',
-                        style: GoogleFonts.rubik(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w100,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 7,
-                      ),
-                      Text(
-                        'I have been working as a co-founder and software developer of Infumia software and graphic design company since 2019.',
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.rubik(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            )
           ],
         ),
         SizedBox(
