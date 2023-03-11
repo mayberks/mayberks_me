1c0a80e8712b5a10c45786ef7a2cfcc906410280
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-11 04:24:12 +0100

    done portfolio section

--- lib/components/dashboard_portfolio_section.dart
+++ lib/components/dashboard_portfolio_section.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:mayberks_me/components/portfolio/discord_setups.dart';
 import 'package:mayberks_me/components/portfolio/flutter_projects.dart';
+import 'package:mayberks_me/components/portfolio/minecraft_configurations.dart';
+import 'package:mayberks_me/components/portfolio/minecraft_optimizations.dart';
+import 'package:mayberks_me/components/portfolio/minecraft_setups.dart';
+import 'package:mayberks_me/components/portfolio/mobile_applications.dart';
 import 'package:mayberks_me/constants/extensions.dart';
 
 class DashboardPortfolioSection extends StatelessWidget {
@@ -8,7 +13,7 @@ class DashboardPortfolioSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: context.dynamicHeight(0.7),
+      height: context.dynamicHeight(0.85),
       child: Row(
         children: [
           const Spacer(
@@ -16,73 +21,78 @@ class DashboardPortfolioSection extends StatelessWidget {
           ),
           Expanded(
             flex: 17,
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(15),
+            child: Column(
+              children: [
+                const Spacer(
+                  flex: 2,
                 ),
-              ),
-              width: context.dynamicWidth(0.5),
-              height: context.dynamicHeight(0.7),
-              child: Column(
-                children: [
-                  const Spacer(
-                    flex: 5,
+                Expanded(
+                  flex: 87,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: context.dynamicWidth(0.005),
+                          ),
+                          const FlutterProjects(),
+                          SizedBox(
+                            width: context.dynamicWidth(0.005),
+                          ),
+                          const MinecraftSetups(),
+                          SizedBox(
+                            width: context.dynamicWidth(0.005),
+                          ),
+                          const MinecraftConfigurations(),
+                          SizedBox(
+                            width: context.dynamicWidth(0.005),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                  Expanded(
-                    flex: 100,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            SizedBox(
-                              width: context.dynamicWidth(0.005),
-                            ),
-                            const FlutterProjects(),
-                            SizedBox(
-                              width: context.dynamicWidth(0.005),
-                            ),
-                            Container(
-                              decoration: const BoxDecoration(
-                                color: Color.fromARGB(255, 40, 40, 40),
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(15),
-                                ),
-                              ),
-                              width: context.dynamicWidth(0.15),
-                              height: context.dynamicHeight(0.4),
-                            ),
-                            SizedBox(
-                              width: context.dynamicWidth(0.005),
-                            ),
-                            Container(
-                              decoration: const BoxDecoration(
-                                color: Color.fromARGB(255, 40, 40, 40),
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(15),
-                                ),
-                              ),
-                              width: context.dynamicWidth(0.15),
-                              height: context.dynamicHeight(0.4),
-                            ),
-                            SizedBox(
-                              width: context.dynamicWidth(0.005),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
+                ),
+                const Spacer(
+                  flex: 1,
+                ),
+                Expanded(
+                  flex: 87,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: context.dynamicWidth(0.005),
+                          ),
+                          const MinecraftOptimizations(),
+                          SizedBox(
+                            width: context.dynamicWidth(0.005),
+                          ),
+                          const DiscordSetups(),
+                          SizedBox(
+                            width: context.dynamicWidth(0.005),
+                          ),
+                          const MobileApplications(),
+                          SizedBox(
+                            width: context.dynamicWidth(0.005),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                  const Spacer(
-                    flex: 5,
-                  ),
-                ],
-              ),
+                ),
+                const Spacer(
+                  flex: 2,
+                ),
+              ],
             ),
           ),
           const Spacer(
