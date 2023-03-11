261081b7422917ea50e0a27efe9f2cf2a2289535
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-11 06:19:18 +0100

    Create web_settings.dart

---
+++ lib/constants/web_settings.dart
@@ -0,0 +1,9 @@
+
+
+class Settings {
+  String githubLink = "https://github.com/mayberks";
+  String linkedIn = "https://www.linkedin.com/in/ayberkcakrs/";
+  String discordLink = "https://discordapp.com/channels/@me/658619270720389120";
+  String whatsApp = "https://wa.me/905522510266";
+  String companyLink = "https://infumia.net";
+}
