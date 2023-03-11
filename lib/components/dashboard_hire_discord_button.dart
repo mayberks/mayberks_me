21198d0bcc6fb445cb5a9b2c6cc71d3639575680
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-11 07:20:15 +0100

    Update dashboard_hire_discord_button.dart

--- lib/components/dashboard_hire_discord_button.dart
+++ lib/components/dashboard_hire_discord_button.dart
@@ -13,59 +13,71 @@ class DashboardHireDiscordButton extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        OutlinedButton.icon(
-          style: OutlinedButton.styleFrom(
-            side: const BorderSide(
-              color: Colors.white,
-            ),
-            minimumSize:
-                Size(context.dynamicWidth(0.13), context.dynamicHeight(0.05)),
-            backgroundColor: Colors.transparent,
-            foregroundColor: Colors.white,
-            textStyle: GoogleFonts.karla(
-              fontWeight: FontWeight.w300,
-              fontSize: 22,
-            ),
-          ),
-          onPressed: () {
-            launchSendMail();
-          },
-          icon: const FaIcon(
-            FontAwesomeIcons.mugHot,
-            color: Color.fromRGBO(255, 251, 245, 1),
-            size: 20,
-          ),
-          label: const Text('Hey, hire me!'),
+        Spacer(
+          flex: 14,
         ),
-        SizedBox(
-          width: context.dynamicWidth(0.05),
+        Expanded(
+          flex: 6,
+          child: OutlinedButton.icon(
+            style: OutlinedButton.styleFrom(
+              side: const BorderSide(
+                color: Colors.white,
+              ),
+              minimumSize:
+                  Size(context.dynamicWidth(0.13), context.dynamicHeight(0.05)),
+              backgroundColor: Colors.transparent,
+              foregroundColor: Colors.white,
+              textStyle: GoogleFonts.karla(
+                fontWeight: FontWeight.w300,
+                fontSize: 22,
+              ),
+            ),
+            onPressed: () {
+              launchSendMail();
+            },
+            icon: const FaIcon(
+              FontAwesomeIcons.mugHot,
+              color: Color.fromRGBO(255, 251, 245, 1),
+              size: 20,
+            ),
+            label: const Text('Hey, hire me!'),
+          ),
         ),
-        OutlinedButton.icon(
-          style: OutlinedButton.styleFrom(
-            side: const BorderSide(
-              color: Colors.white,
+        Spacer(
+          flex: 1,
+        ),
+        Expanded(
+          flex: 6,
+          child: OutlinedButton.icon(
+            style: OutlinedButton.styleFrom(
+              side: const BorderSide(
+                color: Colors.white,
+              ),
+              minimumSize:
+                  Size(context.dynamicWidth(0.13), context.dynamicHeight(0.05)),
+              backgroundColor: Colors.transparent,
+              foregroundColor: Colors.white,
             ),
-            minimumSize:
-                Size(context.dynamicWidth(0.13), context.dynamicHeight(0.05)),
-            backgroundColor: Colors.transparent,
-            foregroundColor: Colors.white,
-          ),
-          onPressed: () {
-            launchDiscord();
-          },
-          icon: const FaIcon(
-            FontAwesomeIcons.discord,
-            color: Color.fromRGBO(88, 101, 242, 1),
-            size: 20,
-          ),
-          label: Text(
-            'ayberkcakr#9861',
-            style: GoogleFonts.karla(
-              fontWeight: FontWeight.w300,
-              fontSize: 22,
+            onPressed: () {
+              launchDiscord();
+            },
+            icon: const FaIcon(
+              FontAwesomeIcons.discord,
+              color: Color.fromRGBO(88, 101, 242, 1),
+              size: 20,
+            ),
+            label: Text(
+              'ayberkcakr#9861',
+              style: GoogleFonts.karla(
+                fontWeight: FontWeight.w300,
+                fontSize: 22,
+              ),
             ),
           ),
-        )
+        ),
+        Spacer(
+          flex: 14,
+        ),
       ],
     );
   }
