84918ce0dd0b77a370244240f3448324da75090f
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-11 02:19:41 +0100

    finish certificates sections

---
+++ lib/components/certificates/web_certificates.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mayberks_me/constants/extensions.dart';
+
+class WebCertificatesTurkey extends StatelessWidget {
+  const WebCertificatesTurkey({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Spacer(
+          flex: 5,
+        ),
+        Expanded(
+          flex: 20,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: const [
+              Expanded(
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: CircleAvatar(
+                    minRadius: 30,
+                    backgroundColor: Colors.black,
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundImage:
+                      AssetImage('assets/images/meb_logo.png'),
+                    ),
+                  ),
+                ),
+              ),
+              Spacer(),
+            ],
+          ),
+        ),
+        const Spacer(
+          flex: 5,
+        ),
+        Expanded(
+          flex: 100,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Turkish Ministry of Education',
+                style: GoogleFonts.karla(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 18,
+                ),
+              ),
+              Text(
+                'Web Development',
+                style: GoogleFonts.karla(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 14,
+                ),
+              ),
+              SizedBox(
+                height: context.dynamicHeight(0.005),
+              ),
+              Text(
+                'I received 6 months of web application development training and certification in Turkey.',
+                style: GoogleFonts.karla(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w200,
+                  fontSize: 14,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Spacer(
+          flex: 1,
+        ),
+      ],
+    );
+  }
+}
--- lib/components/dashboard_details_section.dart
+++ lib/components/dashboard_details_section.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:mayberks_me/components/certificates/c_sharp_certificates.dart';
+import 'package:mayberks_me/components/certificates/web_certificates.dart';
 import 'package:mayberks_me/components/educations/ataturk_university.dart';
 import 'package:mayberks_me/components/educations/vijya_university.dart';
 import 'package:mayberks_me/constants/extensions.dart';
@@ -72,8 +74,37 @@ class DashboardDetailsSection extends StatelessWidget {
                                 flex: 4,
                                 child: VijyaUniversity(),
                               ),
+                              const Divider(
+                                color: Color.fromARGB(255, 20, 20, 20),
+                                thickness: 0.4,
+                              ),
                               const Spacer(
-                                flex: 11,
+                                flex: 1,
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Text(
+                                  'My Certificates',
+                                  style: GoogleFonts.karla(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ),
+                              const Spacer(
+                                flex: 1,
+                              ),
+                              const Expanded(
+                                flex: 4,
+                                child: WebCertificatesTurkey(),
+                              ),
+                              const Spacer(
+                                flex: 1,
+                              ),
+                              const Expanded(
+                                flex: 4,
+                                child: CSharpCertificatesTurkey(),
                               ),
                             ],
                           ),
@@ -97,7 +128,7 @@ class DashboardDetailsSection extends StatelessWidget {
                               Expanded(
                                 flex: 1,
                                 child: Text(
-                                  'My Certificate & Skills',
+                                  'My Skills',
                                   style: GoogleFonts.karla(
                                     color: Colors.white,
                                     fontWeight: FontWeight.w500,
