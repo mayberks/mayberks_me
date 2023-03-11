3ca336aa235897e152252bb7cc375315d0847a87
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-11 06:55:59 +0100

    Update dashboard_footer_image.dart

--- lib/components/dashboard_footer_image.dart
+++ lib/components/dashboard_footer_image.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:mayberks_me/constants/extensions.dart';
+import 'package:mayberks_me/constants/urls.dart';
 
 class DashboardFooterImage extends StatelessWidget {
   const DashboardFooterImage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset(
-      'assets/images/infumia_favicon.png',
-      width: context.dynamicWidth(0.03),
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onTap: () {
+          launchCompany();
+        },
+        child: Image.asset(
+          'assets/images/infumia_favicon.png',
+          width: context.dynamicWidth(0.03),
+        ),
+      ),
     );
   }
 }
