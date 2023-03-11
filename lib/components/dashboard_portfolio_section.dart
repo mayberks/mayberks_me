8ae394e3dffac8033754b0f73d3ac7b01f22a668
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-11 04:05:02 +0100

    portfolio

--- lib/components/dashboard_portfolio_section.dart
+++ lib/components/dashboard_portfolio_section.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mayberks_me/constants/extensions.dart';
 
 class DashboardPortfolioSection extends StatelessWidget {
@@ -32,11 +34,11 @@ class DashboardPortfolioSection extends StatelessWidget {
                   Expanded(
                     flex: 100,
                     child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.start,
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
                             SizedBox(
@@ -44,13 +46,77 @@ class DashboardPortfolioSection extends StatelessWidget {
                             ),
                             Container(
                               decoration: const BoxDecoration(
-                                color: Color.fromARGB(255, 40, 40, 40),
+                                image: DecorationImage(
+                                  fit: BoxFit.fitHeight,
+                                  image: AssetImage(
+                                    'assets/images/flutter_projects6.png',
+                                  ),
+                                ),
                                 borderRadius: BorderRadius.all(
                                   Radius.circular(15),
                                 ),
                               ),
                               width: context.dynamicWidth(0.15),
                               height: context.dynamicHeight(0.4),
+                              child: Column(
+                                children: [
+                                  const Spacer(
+                                    flex: 5,
+                                  ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        print("Tıklandı: Flutter Projets");
+                                      },
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                            color: Colors.white.withOpacity(
+                                                0.2),
+                                            borderRadius: const BorderRadius
+                                                .all(
+                                              Radius.circular(15),
+                                            )),
+                                        child: Row(
+                                          children: [
+                                            const Spacer(
+                                              flex: 1,
+                                            ),
+                                            Expanded(
+                                              flex: 10,
+                                              child: Text(
+                                                'Click to view!',
+                                                style: GoogleFonts.karla(
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.w400,
+                                                  fontSize: 14,
+                                                ),
+                                              ),
+                                            ),
+                                            const Spacer(
+                                              flex: 3,
+                                            ),
+                                            Expanded(
+                                              flex: 2,
+                                              child: IconButton(
+                                                onPressed: () {},
+                                                icon: const FaIcon(
+                                                  FontAwesomeIcons.arrowRight,
+                                                  color: Colors.white,
+                                                  size: 14,
+                                                ),
+                                              ),
+                                            ),
+                                            const Spacer(
+                                              flex: 1,
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                             SizedBox(
                               width: context.dynamicWidth(0.005),
