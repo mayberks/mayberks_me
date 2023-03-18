95e29afb0d25dce89b0723dad2f257251e47213c
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-18 15:19:22 +0100

    added colors

---
+++ lib/components/icon_button.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Widget iconButton(String title, String image) {
+  return Container(
+    
+  );
+}
---
+++ lib/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+/// a theme color section.
+const Color backgroundColor = Color.fromARGB(255, 20, 20, 20);
+const Color containerBackgroundColor = Color.fromARGB(255, 40, 40, 40);
+
+
+/// a normally color section.
+const Color orangeColor = Color.fromRGBO(255, 139, 19, 1);
+const Color whiteColor = Color.fromRGBO(249, 249, 249, 1);
+const Color blueColor = Color.fromRGBO(98, 205, 255, 1);
+const Color greyColor = Color.fromRGBO(189, 205, 214, 1);
