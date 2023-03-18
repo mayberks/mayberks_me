0f5dc78b37c021d01a5ef8708c1a4bd70b9522e1
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-18 15:49:03 +0100

    add wrapper

--- lib/components/wrapper.dart
+++ lib/components/wrapper.dart
@@ -1,1 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mayberks_me/constants/colors.dart';
+
+/// the wrapper.
+Widget wrapper(
+    {required Widget child,
+    double horizontal = 20,
+    double vertical = 20,
+    Color color = backgroundColor}) {
+  return Container(
+    width: Get.width,
+    margin: EdgeInsets.symmetric(
+      horizontal: horizontal,
+      vertical: vertical,
+    ),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+      color: color,
+    ),
+    child: Center(
+      child: child,
+    ),
+  );
+}
