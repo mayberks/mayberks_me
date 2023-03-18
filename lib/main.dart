ea59c08df8d3307ce4697467f7a54e3474c63569
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-18 15:10:35 +0100

    key

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:mayberks_me/ui/home_screen.dart';
-import 'package:responsive_framework/utils/scroll_behavior.dart';
 
 void main() {
   runApp(const MayberksMe());
 }
 
 class MayberksMe extends StatelessWidget {
-  const MayberksMe({Key? key}) : super(key: key);
+  const MayberksMe({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,5 +17,3 @@ class MayberksMe extends StatelessWidget {
     );
   }
 }
-
-
