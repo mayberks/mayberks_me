9ff4674295cfc493e8ca84b52d9e07b150d59706
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-18 17:09:08 +0100

    format comment

--- lib/utilities/profile_data.dart
+++ lib/utilities/profile_data.dart
@@ -14,18 +14,21 @@ class ProfileData {
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'
       's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
 
+  /// the experiences list.
   static List experiences = [
     {
       'image': 'assets/images/infumia_limited_logo.png',
       'title': 'Infumia Limited',
       'sub_title': 'Co-Founder & Developer',
-      'description': 'I have been working as Cheif Operating Officer of Glory Hosting Solutions company since 2019.'
+      'description':
+          'I have been working as Cheif Operating Officer of Glory Hosting Solutions company since 2019.'
     },
     {
       'image': 'assets/images/glory_hosting_solutions.png',
       'title': 'Glory Hosting Solutions',
       'sub_title': 'SADSADASDSAD',
-      'description': 'I have been working as Cheif Operating Officer of Glory Hosting Solutions company since 2019.'
+      'description':
+          'I have been working as Cheif Operating Officer of Glory Hosting Solutions company since 2019.'
     },
   ];
 }
