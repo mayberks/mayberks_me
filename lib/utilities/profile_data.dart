a567866bd79fb5a4e9bf955c96bcd01b2ac0f808
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-18 17:08:53 +0100

    added test profile data

---
+++ lib/utilities/profile_data.dart
@@ -0,0 +1,31 @@
+/// the profile data.
+class ProfileData {
+  /// the profile image link.
+  static String profileImage = 'assets/images/personal_image.png';
+
+  /// the profile name.
+  static String profileName = 'AYBERK CAKIR';
+
+  /// the job title.
+  static String job = 'Developer';
+
+  /// the about me.
+  static String aboutMe =
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'
+      's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
+
+  static List experiences = [
+    {
+      'image': 'assets/images/infumia_limited_logo.png',
+      'title': 'Infumia Limited',
+      'sub_title': 'Co-Founder & Developer',
+      'description': 'I have been working as Cheif Operating Officer of Glory Hosting Solutions company since 2019.'
+    },
+    {
+      'image': 'assets/images/glory_hosting_solutions.png',
+      'title': 'Glory Hosting Solutions',
+      'sub_title': 'SADSADASDSAD',
+      'description': 'I have been working as Cheif Operating Officer of Glory Hosting Solutions company since 2019.'
+    },
+  ];
+}
