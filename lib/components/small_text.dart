c232961078a9c8a931b4d7f526673276ee489cfd
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-18 15:58:16 +0100

    add small text

---
+++ lib/components/small_text.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/widgets.dart';
+import 'package:mayberks_me/constants/colors.dart';
+import 'package:mayberks_me/utilities/responsive.dart';
+
+Widget smallText(title, BuildContext context,
+    {TextAlign align = TextAlign.start,
+    FontWeight weight = FontWeight.w100,
+    Color color = whiteColor,
+    TextOverflow? overflow,
+    int? maxLines,
+    double size = 10}) {
+  return Text(
+    title,
+    textAlign: align,
+    maxLines: maxLines,
+    overflow: overflow,
+    style: TextStyle(
+      wordSpacing: 1,
+      fontSize: isDesktop(context)
+          ? size * 1.5
+          : isBigDesktop(context)
+              ? size * 2
+              : size,
+      fontWeight: weight,
+      color: color,
+    ),
+  );
+}
