ad75d2c9efc1c28df419e287198d240abbc6b917
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-18 15:27:18 +0100

    add height

--- lib/constants/spaces.dart
+++ lib/constants/spaces.dart
@@ -1,5 +1,51 @@
 import 'package:flutter/widgets.dart';
 
-/// a height sections.const h2 = SizedBox(
+/// the 2 height sizedbox.
+const heightTwo = SizedBox(
   height: 2,
+);
+
+/// the 3 height sizedbox.
+const heightTree = SizedBox(
+  height: 3,
+);
+
+/// the 5 height sizedbox.
+const heightFour = SizedBox(
+  height: 5,
+);
+
+/// the 10 height sizedbox.
+const heightTen = SizedBox(
+  height: 10,
+);
+
+/// the 15 height sizedbox.
+const heightFifteen = SizedBox(
+  height: 15,
+);
+
+/// the 20 height sizedbox.
+const heightTwenty = SizedBox(
+  height: 20,
+);
+
+/// the 25 height sizedbox.
+const heightTwentyFive = SizedBox(
+  height: 25,
+);
+
+/// the 30 height sizedbox.
+const heightThirty = SizedBox(
+  height: 30,
+);
+
+/// the 40 height sizedbox.
+const heightForty = SizedBox(
+  height: 40,
+);
+
+/// the 50 height sizedbox.
+const heightFifty = SizedBox(
+  height: 50,
 );
