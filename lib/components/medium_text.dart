da86eb7ac1c5c750bd28becccddf1dead2cc541c
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-18 16:06:30 +0100

    Update medium_text.dart

--- lib/components/medium_text.dart
+++ lib/components/medium_text.dart
@@ -4,6 +4,7 @@ import 'package:flutter/widgets.dart';
 import 'package:mayberks_me/constants/colors.dart';
 import 'package:mayberks_me/utilities/responsive.dart';
 
+/// the medium text.
 Widget mediumText(title, BuildContext context,
     {TextAlign align = TextAlign.start,
     FontWeight weight = FontWeight.w500,
