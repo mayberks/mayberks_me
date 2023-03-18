989dcca56606a45bb5ce7dc00e8c820887279f73
Ayberk ÇAKIR <71984897+mayberks@users.noreply.github.com>
2023-03-18 16:59:40 +0100

    add heading

---
+++ lib/components/heading.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/widgets.dart';
+import 'package:mayberks_me/constants/colors.dart';
+import 'package:mayberks_me/utilities/responsive.dart';
+
+Widget heading(title, BuildContext context,
+    {TextAlign align = TextAlign.start,
+    FontWeight weight = FontWeight.w700,
+    Color color = whiteColor,
+    double size = 16}) {
+  return Text(
+    title,
+    textAlign: align,
+    style: TextStyle(
+      fontSize: isDesktop(context)
+          ? size * 1.5
+          : isBigDesktop(context)
+              ? size * 2
+              : size,
+      fontWeight: weight,
+      color: color,
+    ),
+  );
+}
--- lib/components/rounded_image.dart
+++ lib/components/rounded_image.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:mayberks_me/utilities/responsive.dart';
 
-
 /// the rounded image.
-Widget roundedImage(BuildContext context,
-    {required String image, double radius = 20, double size = 100}) {
+Widget roundedImage(
+  BuildContext context, {
+  required String image,
+  double radius = 20,
+  double size = 100,
+}) {
   return ClipRRect(
     borderRadius: BorderRadius.circular(radius),
     child: Image.asset(
